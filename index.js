3cb1cda414572b8a9767f00b9eff18463a514e85
Miguel Nazario <mnazario35@gmail.com>
2022-01-31 19:55:47 -0500

    commit tree df9b2e3e295b7cd48981d39ffc4fba266578d2b2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function returnFirstTwoDrivers(array) {
+    return array.slice(0,2);
+};
+
+const returnLastTwoDrivers = function returnLastTwoDrivers(array) {
+    return array.slice(array.length-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function fareTripler(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+const selectDifferentDrivers = function selectDifferentDrivers(arrayOfDrivers, either) {
+    return either(arrayOfDrivers);
+}
